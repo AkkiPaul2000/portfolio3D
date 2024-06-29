4e2401cd6ecd74516ad0e1fe27a667d10a1b142d
akkipaul2000 <skypaul2000@gmail.com>
2024-06-29 12:43:35 +0530

    3d cube solved I wanted to be allsided text together

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,39 +1,38 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Text } from '@react-three/drei';
-
-const Container = styled.div`
-  width: 100%;
-  height: 100vh;
-  scroll-snap-align: center;
-  /* background-color: royalblue; */
-  color: white;
-`;
+import React from 'react'
+import styled from 'styled-components'
+import {Canvas} from '@react-three/fiber'
+import {OrbitControls, PerspectiveCamera, RenderTexture,Text} from '@react-three/drei'
 
 function Test() {
+    const Container=styled.div`
+        width: 100%;
+        height: 100vh;
+        scroll-snap-align: center;
+      /* background-color: royalblue ; */
+      color: white;
+    `
+
   return (
     <Container>
-      <Canvas>
-        <OrbitControls enableZoom={false} />
-        <ambientLight intensity={1} />
-        <directionalLight position={[3, 2, 1]} />
-        <mesh>
-          <boxGeometry args={[1, 1, 1]} />
-          <meshStandardMaterial color={0x00ff00} />
-          <Text 
-            position={[0, 0, 0.51]} 
-            fontSize={0.2} 
-            color="#555"
-            anchorX="center" 
-            anchorY="middle"
-          >
-            hello
-          </Text>
-        </mesh>
-      </Canvas>
+        <Canvas>
+          <OrbitControls enableZoom={false} />
+            <mesh>
+              <boxGeometry args={[1,1,1]}/>
+              <ambientLight intensity={1} />
+              <directionalLight position={[3,2,1]}/> 
+              <meshStandardMaterial color={0x00ff00} >
+              <RenderTexture attach="map">
+                <PerspectiveCamera makeDefault position={[0,0,5]} />
+                <color attach="background" args={["#dc9dcd"]}/>
+                <Text fontSize={3} color="#555">hello</Text>
+              </RenderTexture>
+              </meshStandardMaterial>
+                    {/* <meshStandardMaterial color={0x00ff00} roughness={0.5} metalness={0.5} /> */}
+
+            </mesh>
+        </Canvas>
     </Container>
-  );
+  )
 }
 
-export default Test;
+export default Test
